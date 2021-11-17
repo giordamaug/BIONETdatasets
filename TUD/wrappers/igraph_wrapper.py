7c49bd00ab6aad8b97c5108c97099bf917f2e603
Maurizio Giordano <maurizio.giordano@cnr.it>
2021-11-18 00:13:50 +0100

    igraph wrapper

---
+++ TUD/wrappers/igraph_wrapper.py
@@ -0,0 +1,162 @@
+from igraph import Graph
+import numpy as np
+
+class MyTUDataset(DGLBuiltinDataset):
+    r"""
+    MyTUDataset contains lots of graph kernel datasets for graph classification.
+
+    Parameters
+    ----------
+    name : str
+        Dataset Name, such as ``ENZYMES``, ``DD``, ``COLLAB``, ``MUTAG``, can be the 
+        datasets name on `<https://chrsmrrs.github.io/datasets/docs/datasets/>`_.
+    path : str
+        Dataset directory path.
+
+    Attributes
+    ----------
+    max_num_node : int
+        Maximum number of nodes
+    num_labels : int
+        Number of classes
+
+    Notes
+    -----
+    **IMPORTANT:** Some of the datasets have duplicate edges exist in the graphs, e.g.
+    the edges in ``IMDB-BINARY`` are all duplicated.  DGL faithfully keeps the duplicates
+    as per the original data.  Other frameworks such as PyTorch Geometric removes the
+    duplicates by default.  You can remove the duplicate edges with :func:`dgl.to_simple`.
+
+    """
+
+    def __init__(self, name, path, raw_dir=None, force_reload=False, verbose=False):
+        self.path = path
+        super(MyTUDataset, self).__init__(name=name, url="",
+                                        raw_dir=raw_dir, force_reload=force_reload,
+                                        verbose=verbose)
+    
+    def download(self):
+        pass
+
+    def process(self):
+        DS_edge_list = self._idx_from_zero(
+            loadtxt(self._file_path("A"), delimiter=",").astype(int))
+        DS_indicator = self._idx_from_zero(
+            loadtxt(self._file_path("graph_indicator"), delimiter=",").astype(int))
+        
+        if os.path.exists(self._file_path("graph_labels")):
+            DS_graph_labels = self._idx_reset(
+                loadtxt(self._file_path("graph_labels"), delimiter=",").astype(int))               
+            self.num_labels = max(DS_graph_labels) + 1
+            self.graph_labels = F.tensor(DS_graph_labels)     
+        elif os.path.exists(self._file_path("graph_attributes")):
+            DS_graph_labels = loadtxt(self._file_path("graph_attributes"), delimiter=",").astype(float)
+            self.num_labels = None
+            self.graph_labels = F.tensor(DS_graph_labels)     
+        else:
+            raise Exception("Unknown graph label or graph attributes")
+
+        g = Graph(()
+        g.add_vertices(np.max(DS_indicator) + 1)
+        g.add_edges(DS_edge_list[:, 0], DS_edge_list[:, 1])
+
+        node_idx_list = []
+        self.max_num_node = 0
+        for idx in range(np.max(DS_indicator) + 1):
+            node_idx = np.where(DS_indicator == idx)
+            node_idx_list.append(node_idx[0])
+            if len(node_idx[0]) > self.max_num_node:
+                self.max_num_node = len(node_idx[0])
+
+
+        self.attr_dict = {
+            'node_labels': ('ndata', 'node_labels'),
+            'node_attributes': ('ndata', 'node_attr'),
+            'edge_labels': ('edata', 'edge_labels'),
+            'edge_attributes': ('edata', 'node_labels'),
+        }
+
+        for filename, field_name in self.attr_dict.items():
+            try:
+                data = loadtxt(self._file_path(filename),
+                               delimiter=',')
+                if 'label' in filename:
+                    data = F.tensor(self._idx_from_zero(data))
+                else:
+                    data = F.tensor(data)
+                getattr(g, field_name[0])[field_name[1]] = data
+            except IOError:
+                pass
+
+        self.graph_lists = [g.subgraph(node_idx) for node_idx in node_idx_list]
+
+    def save(self):
+        graph_path = os.path.join(self.save_path, 'tu_{}.bin'.format(self.name))
+        info_path = os.path.join(self.save_path, 'tu_{}.pkl'.format(self.name))
+        label_dict = {'labels': self.graph_labels}
+        info_dict = {'max_num_node': self.max_num_node,
+                     'num_labels': self.num_labels}
+        save_graphs(str(graph_path), self.graph_lists, label_dict)
+        save_info(str(info_path), info_dict)
+
+    def load(self):
+        graph_path = os.path.join(self.save_path, 'tu_{}.bin'.format(self.name))
+        info_path = os.path.join(self.save_path, 'tu_{}.pkl'.format(self.name))
+        graphs, label_dict = load_graphs(str(graph_path))
+        info_dict = load_info(str(info_path))
+
+        self.graph_lists = graphs
+        self.graph_labels = label_dict['labels']
+        self.max_num_node = info_dict['max_num_node']
+        self.num_labels = info_dict['num_labels']
+
+    def has_cache(self):
+        graph_path = os.path.join(self.save_path, 'tu_{}.bin'.format(self.name))
+        info_path = os.path.join(self.save_path, 'tu_{}.pkl'.format(self.name))
+        if os.path.exists(graph_path) and os.path.exists(info_path):
+            return True
+        return False
+
+    def __getitem__(self, idx):
+        """Get the idx-th sample.
+
+        Parameters
+        ---------
+        idx : int
+            The sample index.
+
+        Returns
+        -------
+        (:class:`dgl.DGLGraph`, Tensor)
+            Graph with node feature stored in ``feat`` field and node label in ``node_label`` if available.
+            And its label.
+        """
+        g = self.graph_lists[idx]
+        return g, self.graph_labels[idx]
+
+
+    def __len__(self):
+        """Return the number of graphs in the dataset."""
+        return len(self.graph_lists)
+
+
+    def _file_path(self, category):
+        return os.path.join(self.path, self.name,
+                            "{}_{}.txt".format(self.name, category))
+
+    @staticmethod
+    def _idx_from_zero(idx_tensor):
+        return idx_tensor - np.min(idx_tensor)
+
+    @staticmethod
+    def _idx_reset(idx_tensor):
+        """Maps n unique labels to {0, ..., n-1} in an ordered fashion."""
+        labels = np.unique(idx_tensor)
+        relabel_map = {x: i for i, x in enumerate(labels)}
+        new_idx_tensor = np.vectorize(relabel_map.get)(idx_tensor)
+        return new_idx_tensor
+
+    def statistics(self):
+        return self.graph_lists[0].ndata['feat'].shape[1], \
+            self.num_labels, \
+            self.max_num_node
